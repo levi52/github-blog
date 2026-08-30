24ad49ca6237edcf93fe7b3ef3467f426b96ae08
levi52 <hillviolet153@163.com>
2026-08-30 20:42:27 +0800

    fix: 修复sitemap和robots.txt的basePath路径

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 const config = {
-  siteUrl: "https://levi52.github.io",
+  siteUrl: "https://levi52.github.io/github-blog",
   generateRobotsTxt: true,
   sitemapSize: 7000,
 };
